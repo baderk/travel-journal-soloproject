aa08d05284eacfe3e95a70c98d308dd3528c8583
Bader Kanawati <baderkanawati@BaderM1.local>
2022-11-21 00:42:02 -0400

    media queries, content, all the styling

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -1,20 +1,26 @@
-
 const DestinationCard = (props) => {
-
   return (
     <div className="card">
-      <img src={props.item.imageUrl} alt={props.item.title} className="card--image" />
+      <img
+        src={props.item.imageUrl}
+        alt={props.item.title}
+        className="card--image"
+      />
       <div className="card--info">
         <section className="card--location">
           <p className="card--location-name">{props.item.location}</p>
-          <a className="card--location-link" href={props.item.googleMapsUrl}>View on Google Maps</a>
+          <a className="card--location-link" href={props.item.googleMapsUrl}>
+            View on Google Maps
+          </a>
         </section>
         <h2 className="card--title">{props.item.title}</h2>
-        <p className="card--date">{props.item.startDate} - {props.item.endDate}</p>
+        <p className="card--date">
+          {props.item.startDate} - {props.item.endDate}
+        </p>
         <p className="card--description">{props.item.description}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DestinationCard;
--- src/data.js
+++ src/data.js
@@ -7,8 +7,9 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
     id: 2,
@@ -17,8 +18,9 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-    imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+    imageUrl: "https://source.unsplash.com/JmuyB_LibRo",
   },
   {
     id: 3,
@@ -27,8 +29,9 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "The GeirrangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.,angerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    description:
+      "The Geirranger The Geirange r The GeirangerThe Geira   ngerThe Geiranger The Geiranger  The  GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal      county, Norway. It is located entirely in the Stranda Municipality.,angerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe GeirangerThe Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
   {
     id: 4,
@@ -37,8 +40,9 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
   {
     id: 5,
@@ -47,8 +51,9 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
   {
     id: 6,
@@ -57,7 +62,8 @@ export default [
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
-    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
-]
+];
