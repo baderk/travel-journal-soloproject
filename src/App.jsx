a938df7d888fb23878e918c049643f2e94988de3
Bader Kanawati <baderkanawati@m1pro.pivpn>
2022-11-21 12:24:06 -0400

    setting the themecontext, chose dark colors and setup IDs for light and dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import Navbar from "./components/Navbar";
 import DestinationCard from "./components/DestinationCard";
-import data from './data';
+import data from "./data";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const posts = data.map((item) => {
+    return <DestinationCard key={item.id} item={item} />;
+  });
 
-  const posts = data.map(item => {
-    return <DestinationCard
-      key={item.id}
-      item={item}
-    />
-  })
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
 
   return (
-    <div className="container">
-      <Navbar />
-      <section className="posts">
-        {posts}
-      </section>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="container" id={theme}>
+        <Navbar />
+        <section className="posts">{posts}</section>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
